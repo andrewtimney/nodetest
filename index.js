03266ee2ea6427b15e95451f752bafb448dc1808
Andrew Timney <andrew.timney@gmail.com>
2015-10-25 22:10:00 +0000

    Edit camera code

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var RaspiCam = require("raspicam");
 
-var camera = new RaspiCam({ mode:'photo', output:'test.jpg' });
+var camera = new RaspiCam({ mode:'photo', output:'./stest.jpg' });
 
 //to take a snapshot, start a timelapse or video recording
-camera.start( );
+camera.start();
 
 //to stop a timelapse or video recording
 
 
 //listen for the "start" event triggered when the start method has been successfully initiated
-camera.on("start", function(){
-    //do stuff
-	console.log('started');
-});
-
-camera.on("read", function(err, timestamp, filename){ 
-    //do stuffs
-	console.log('read');
-});
-
-camera.stop();
+// camera.on("start", function(){
+//     //do stuff
+// 	console.log('started');
+// });
+// 
+// camera.on("read", function(err, timestamp, filename){ 
+//     //do stuffs
+// 	console.log('read');
+// });
+// 
+// camera.stop();
