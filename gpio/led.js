1be72af82716f77cbc4391e0276f199b4faaf743
Andrew Timney <andrew.timney@gmail.com>
2015-10-28 22:26:59 +0000

    Flick led on off

--- gpio/led.js
+++ gpio/led.js
@@ -2,8 +2,15 @@ var Gpio = require("onoff").Gpio;
 
 var led = new Gpio(11, 'out');
 
-led.writeSync(0);
+
 
 setTimeout(function(){
-	led.writeSync(1);s
+	led.writeSync(1);
+	setTimeout(function(){
+		led.write(0, function(er, val){
+			console.log(er, val);
+			led.unexport();
+		});
+	});
 }, 10000);
+
