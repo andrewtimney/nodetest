a99a7dfb71b47eea5943ac25be5573e045146b6e
Andrew Timney <andrew.timney@gmail.com>
2015-11-01 15:34:05 +0000

    Add led into button

--- gpio/button_led.js
+++ gpio/button_led.js
@@ -1,8 +1,12 @@
 var Gpio = require("onoff").Gpio;
 
-var pin = 18;
-var button = new Gpio(pin, 'in', 'both');
+var buttonPin = 18;
+var button = new Gpio(buttonPin, 'in', 'both');
+var ledPin = 17;// GPIO17
+var led = new Gpio(ledPin, 'out');
+console.log('Led is: ', led.readSync());
 
 button.watch(function(er, value){
 	console.log('button changed', value);
+	led.writeSynce(value);
 });
