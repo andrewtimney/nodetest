91a514237f8b8cab155d258a2ba7ff4595e8a224
Andrew Timney <andrew.timney@gmail.com>
2015-11-01 16:05:51 +0000

    Some tidy up

--- gpio/blink_led.js
+++ gpio/blink_led.js
@@ -3,9 +3,16 @@ var Gpio = require("onoff").Gpio;
 var pin = 17;// GPIO17
 var led = new Gpio(pin, 'out');
 
-
 var value = false;
-setInterval(function(){
+var interval = setInterval(function(){
 	led.writeSync(value ? 0 : 1);
 	value = !value;
 }, 500);
+
+function exit() {
+	clearInterval(interval);
+  led.unexport();
+  process.exit();
+}
+ 
+process.on('SIGINT', exit);
