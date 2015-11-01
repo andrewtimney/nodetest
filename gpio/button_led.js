fd57973daa26801a3918ff8e18cd52f25996dadd
Andrew Timney <andrew.timney@gmail.com>
2015-11-01 15:51:09 +0000

    Added some tidyup

--- gpio/button_led.js
+++ gpio/button_led.js
@@ -4,9 +4,16 @@ var buttonPin = 18;
 var button = new Gpio(buttonPin, 'in', 'both');
 var ledPin = 17;// GPIO17
 var led = new Gpio(ledPin, 'out');
-console.log('Led is: ', led.readSync());
 
 button.watch(function(er, value){
 	console.log('button changed', value);
 	led.writeSynce(value);
 });
+
+function exit() {
+  button.unexport();
+  led.unexport();
+  process.exit();
+}
+ 
+process.on('SIGINT', exit);
